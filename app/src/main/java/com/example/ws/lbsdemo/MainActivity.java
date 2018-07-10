9300125167f94155f3e2377d8b2b6461bac217d5
WS <2421275103@qq.com>
2018-07-10 13:35:22 +0800

    First commit

---
+++ app/src/main/java/com/example/ws/lbsdemo/MainActivity.java
@@ -0,0 +1,179 @@
+package com.example.ws.lbsdemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] permissions;
+    private TextView positionText;
+
+    //1.初始化LocationClient类
+    public LocationClient mLocationClient = null;
+    //BDAbstractLocationListener为7.2版本新增的Abstract类型的监听接口
+    //原有BDLocationListener接口暂时同步保留。具体介绍请参考后文中的说明
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        //注册监听函数
+        setContentView(R.layout.activity_main);
+        positionText = (TextView) findViewById(R.id.position_text_view);
+
+        //利用List集合存储3个权限，p389，最后转换成数组，再调用requestPermission()方法一次性申请
+
+        List<String> permissonList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissonList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissonList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissonList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissonList.isEmpty()) {
+            permissions = permissonList.toArray(new String[permissonList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        } else {
+            requestLocation();
+        }
+
+        //2.配置SDK定位参数
+        LocationClientOption option = new LocationClientOption();
+
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，设置定位模式，默认高精度
+        //LocationMode.Hight_Accuracy：高精度；
+        //LocationMode. Battery_Saving：低功耗；
+        //LocationMode. Device_Sensors：仅使用设备；
+
+        option.setCoorType("bd09ll");
+        //可选，设置返回经纬度坐标类型，默认gcj02
+        //gcj02：国测局坐标；
+        //bd09ll：百度经纬度坐标；
+        //bd09：百度墨卡托坐标；
+        //海外地区定位，无需设置坐标类型，统一返回wgs84类型坐标
+
+        option.setScanSpan(1000);
+        //可选，设置发起定位请求的间隔，int类型，单位ms
+        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
+        //如果设置非0，需设置1000ms以上才有效
+
+        option.setOpenGps(true);
+        //可选，设置是否使用gps，默认false
+        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
+
+        option.setLocationNotify(true);
+        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+
+        option.setIgnoreKillProcess(false);
+        //可选，定位SDK内部是一个service，并放到了独立进程。
+        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+
+        option.SetIgnoreCacheException(false);
+        //可选，设置是否收集Crash信息，默认收集，即参数为false
+
+        option.setWifiCacheTimeOut(5 * 60 * 1000);
+        //可选，7.2版本新增能力
+        //如果设置了该接口，首次启动定位时，会先判断当前WiFi是否超出有效期，若超出有效期，会先重新扫描WiFi，然后定位
+
+        option.setEnableSimulateGps(false);
+        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+
+        mLocationClient.setLocOption(option);
+        //mLocationClient为第二步初始化过的LocationClient对象
+        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+    }
+
+    //请求位置
+    private void requestLocation() {
+        mLocationClient.start();
+    }
+
+    //循环将申请的每个权限都进行判断，如果有任何一个权限被拒绝，那么直接调用finish()方法关闭当前程序，所有都同意了就会调用requestLocation()方法一次性申请
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "必须同意所有权限才能使用本程序", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                } else {
+                    Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    //3.实现BDAbstractLocationListener接口
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(final BDLocation location) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    StringBuilder currentPosition = new StringBuilder();
+
+                    //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+                    //以下只列举部分获取经纬度相关（常用）的结果信息
+                    //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+                    double latitude = location.getLatitude();    //获取纬度信息
+                    double longitude = location.getLongitude();    //获取经度信息
+                    float radius = location.getRadius();    //获取定位精度，默认值为0.0f
+                    String coorType = location.getCoorType();
+                    //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+                    int errorCode = location.getLocType();
+                    //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+
+                    //把获得的结果显示出来
+                    currentPosition.append("维度:").append(latitude).append("\n");
+                    currentPosition.append("经度:").append(longitude).append("\n");
+                    currentPosition.append("定位精度:").append(radius).append("\n");
+                    currentPosition.append("经纬度坐标类型:").append(coorType).append("\n");
+                    currentPosition.append("定位错误返回码:").append(errorCode).append("\n");
+                    currentPosition.append("定位方式：");
+                    if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                        currentPosition.append("GPS");
+                    } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                        currentPosition.append("网络");
+                    }
+                    positionText.setText(currentPosition);
+                }
+            });
+        }
+
+
+    }
+}
+
+
